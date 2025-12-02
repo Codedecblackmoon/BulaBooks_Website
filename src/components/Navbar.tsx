fdb9ac51dbebd8a2472087420e88f095e7b679ad
Codedecoblack <gideeonlenge96@gail.com>
2025-12-02 22:45:34 +0200

    fixing download

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,42 @@
 import React from "react";
 import { Menu, X } from "lucide-react";
 import logo1 from '../images/logo1.png'
+import Swal from "sweetalert2";
 import "./Navbar.css";
 
 const Navbar: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const triggerDownload = (filePath, fileName) => {
+    const link = document.createElement("a");
+    link.href = filePath;
+    link.download = fileName;
+    link.click();
+  };
+
+  const handleDownload = () => {
+    Swal.fire({
+      title: "Choose your download",
+      text: "Which version do you want to download?",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonText: "PC Version",
+      cancelButtonText: "Phone Version",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        triggerDownload("/downloads/game-pc.zip", "game-pc.zip");
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        triggerDownload("/downloads/game-phone.apk", "game-phone.apk");
+      }
+    });
+  };
+
+  // const handleDownload = () => {
+  //   const link = document.createElement("a");
+  //   link.href = "/example.pdf"; // file location in public folder
+  //   link.download = "BulaBooks"; 
+  //   link.click();
+  // };
 
   return (
     <header className="header">
@@ -26,7 +58,7 @@ const Navbar: React.FC = () => {
           {/* Desktop Auth Buttons */}
           <div className="auth-desktop">
             {/* <button className="btn-signin">Sign In</button> */}
-            <button className="btn-signup"><a href="#contact">Contect Us</a></button>
+            <button onClick={handleDownload} className="btn-signup"><a >Download </a></button>
           </div>
 
           {/* Mobile menu button */}
